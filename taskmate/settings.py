91d3c23f53d3964e02aec563a53721e31ede7a06
devaraj <devarajs.2k35@gmail.com>
2025-03-11 14:54:08 +0530

    domain updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -5,6 +5,7 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env()
+PORT = os.environ.get('PORT', '8000')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmateproject18.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['taskmateproject18.up.railway.app']
 
 
 # Application definition
